93b4e66fb49ef86f6b1a853293f1db65722673f2
kevin huntly <kmhuntly@gmail.com>
2025-06-02 12:36:23 -0400

    updates
    Signed-off-by: kevin huntly <kmhuntly@gmail.com>

--- lib/system/logging.sh
+++ lib/system/logging.sh
@@ -21,10 +21,7 @@ if [[ -n "${LOGGING_PROPERTIES}" ]]; then
     # shellcheck source=/dev/null
     source "${LOGGING_PROPERTIES}";
 else
-    if [[ -r "${SCRIPT_ROOT}/config/system/logging.properties" ]] && [[ -s "${SCRIPT_ROOT}/config/system/logging.properties" ]]; then
-        # shellcheck source=../../config/system/logging.properties
-        source "${SCRIPT_ROOT}/config/system/logging.properties";
-    elif [[ -r "${HOME}/config/system/logging.properties" ]] && [[ -s "${HOME}/config/system/logging.properties" ]]; then
+    if [[ -r "${HOME}/config/system/logging.properties" ]] && [[ -s "${HOME}/config/system/logging.properties" ]]; then
         source "${HOME}/config/system/logging.properties"; ## if its here, override the above and use it
     elif [[ -r "/usr/local/config/logging.properties" ]] && [[ -s "/usr/local/config/logging.properties" ]]; then
         source "/usr/local/config/logging.properties"; ## if its here, use it
