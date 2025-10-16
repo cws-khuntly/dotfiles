669fe6ca8b024d8602356edba466c064aea99f51
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2025-10-15 21:47:17 -0400

    Add 00-applyset.sh for base system functions
    Initial creation of 00-applyset.sh with base system functions and settings.

---
+++ src/main/shell/bashrc.d/00-applyset.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+#          FILE:  00-applyset.sh
+#         USAGE:  Import file into script and call relevant functions
+#   DESCRIPTION:  Base system functions that don't necessarily belong elsewhere
+#
+#       OPTIONS:  See usage section
+#  REQUIREMENTS:  bash 4+
+#          BUGS:  ---
+#         NOTES:
+#        AUTHOR:  Kevin Huntly <kmhuntly@gmail.com>
+#       COMPANY:  ---
+#       VERSION:  1.0
+#       CREATED:  ---
+#      REVISION:  ---
+#==============================================================================
+
+set -e;
+
+set -o pipefail;
+set -o notify;
+set -o monitor;
+set -o bell-visible-style;
