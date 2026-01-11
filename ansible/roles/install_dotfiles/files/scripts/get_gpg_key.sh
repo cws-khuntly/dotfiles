1af8da390fafc35c4ebb059e25bb8839749fcc00
Kevin Huntly <kmhuntly@gmail.com>
2026-01-10 21:06:33 -0500

    Don’t even try to refactor it.
    Signed-off-by: Kevin Huntly <kmhuntly@gmail.com>

---
+++ ansible/roles/install_dotfiles/files/scripts/get_gpg_key.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+#          FILE:  basefunctions.sh
+#         USAGE:  Import file into script and call relevant functions
+#   DESCRIPTION:  Base system functions that don't necessarily belong elsewhere
+#
+#       OPTIONS:  See usage section
+#  REQUIREMENTS:  bash 4+
+#          BUGS:  ---
+#         NOTES:
+#        AUTHOR:  Kevin Huntly <kmhuntly@gmail.com>
+#       COMPANY:  ---
+#       VERSION:  1.0
+#       CREATED:  ---
+#      REVISION:  ---
+#==============================================================================
+
+#=====  FUNCTION  =============================================================
+#          NAME:  isNaN
+#   DESCRIPTION:  Checks if the provided variable is a number
+#    PARAMETERS:  Variable to check
+#       RETURNS:  0 if true, 1 if false
+#==============================================================================
+printf "%s\n" "$(gpg --list-secret-keys --keyid-format=long | grep "[S]" | cut -d "/" -f 2 | awk '{print $1}')"
+
+return 0;
